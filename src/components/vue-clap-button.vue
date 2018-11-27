7f4cf1ac5a08efb4368d378178ecbd66d30081ec
baker <735267452@qq.com>
2018-11-27 09:39:00 +0800

    one click style

--- src/components/vue-clap-button.vue
+++ src/components/vue-clap-button.vue
@@ -1,7 +1,10 @@
 <template>
-  <div ref="circle" @click="click" class="vclapbtn-bin animated">
-    <div ref="btn" class="animated">
-        <icon-base :iconColor="iconColor">
+  <div ref="circle" @click="click" @mouseover="mouseover" @mouseleave="mouseleave"
+       class="vclapbtn-bin animated"
+       :style="{boxShadow : isHover || isActive ? `${iconColor} 0 0 10px` : null, borderColor : iconColor}"
+       :class="{pulse: clicking}">
+    <div ref="btn" class="animated" :class="{tada: clicking}">
+      <icon-base :iconColor="iconColor">
         <icon-love/>
       </icon-base>
     </div>
@@ -15,11 +18,36 @@
 
   export default {
     name: "vueClapButton",
+    props: {
+      iconColorActive: {
+        type: String,
+        default: "#F56C6C"
+      },
+      iconColorNormal: {
+        type: String,
+        default: "#909399"
+      },
+      maxClick: {
+        type: Number,
+        default: 1
+      }
+    },
     data() {
       return {
-        iconColorActive: "#FF4500",
-        iconColorNormal: "#ccc",
-        iconColor: "#ccc"
+        isHover: false,
+        clicking: false,
+        clickedTime: 0,
+      }
+    },
+    computed: {
+      isActive() {
+        return this.clickedTime > 0;
+      },
+      iconColor() {
+        return this.isActive || this.clicking ? this.iconColorActive : this.iconColorNormal;
+      },
+      isReachMaxClick() {
+        return this.clickedTime >= this.maxClick;
       }
     },
     components: {
@@ -27,19 +55,38 @@
       IconBase,
     },
     methods: {
+      mouseleave() {
+        this.isHover = false;
+      },
+      mouseover() {
+        this.isHover = true;
+      },
       click() {
-        let el = this.$refs.circle;
-        let btn = this.$refs.btn;
-        util.addClass(el, 'pulse');
-        util.addClass(btn, 'tada');
-        this.iconColor = this.iconColorActive;
+        if (this.canClick()) {
+          if (this.maxClick === 1 && this.isReachMaxClick) {
+            this.cancelLove();
+          } else {
+            this.love();
+          }
+        }
+      },
+      cancelLove() {
+        this.clickedTime = 0;
+      },
+      love() {
+        this.clicking = true;
 
         setTimeout(() => {
-          util.removeClass(el, 'pulse');
-          util.removeClass(btn, 'tada');
-          this.iconColor = this.iconColorNormal;
+          this.clickedTime ++;
+          this.clicking = false;
         }, 1000)
-      }
+      },
+      canClick() {
+        if (this.maxClick === 1) {
+          return true;
+        }
+        return this.clickedTime < this.maxClick;
+      },
     }
   }
 </script>
@@ -48,18 +95,15 @@
   .vclapbtn-bin {
     width: 50px;
     height: 50px;
-    cursor:pointer;
+    cursor: pointer;
     line-height: 50px;
     border: 1px solid #ccc;
     border-radius: 50%;
     text-align: center;
   }
+
   .vclapbtn-bin:hover {
-    border-color:orangered;
-    transition:all 1s;
-    -webkit-box-shadow: orangered 0 0 10px;
-    -moz-box-shadow: orangered 0 0 10px;
-    box-shadow: orangered 0 0 10px;
+    transition: all 1s;
   }
 
   @keyframes pulse {
@@ -107,22 +151,21 @@
     animation-name: swing
   }
 
-
   @keyframes tada {
     0% {
       transform: scaleX(1)
     }
 
-    10%,20% {
-      transform: scale3d(.8,.8,.8) rotate(-3deg)
+    10%, 20% {
+      transform: scale3d(.8, .8, .8) rotate(-3deg)
     }
 
-    30%,50%,70%,90% {
-      transform: scale3d(1.2,1.2,1.2) rotate(3deg)
+    30%, 50%, 70%, 90% {
+      transform: scale3d(1.2, 1.2, 1.2) rotate(3deg)
     }
 
-    40%,60%,80% {
-      transform: scale3d(1.2,1.2,1.2) rotate(-3deg)
+    40%, 60%, 80% {
+      transform: scale3d(1.2, 1.2, 1.2) rotate(-3deg)
     }
 
     to {
@@ -134,7 +177,6 @@
     animation-name: tada
   }
 
-
   .animated {
     animation-duration: 1s;
     animation-fill-mode: both;
@@ -144,8 +186,4 @@
     animation-iteration-count: infinite;
   }
 
-
-  .btn.active {
-    color:orangered;
-  }
 </style>
